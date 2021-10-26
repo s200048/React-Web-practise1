37defbc8349c4e8252968de25ff4e49f590265b5
Howard <mhw19972013@gmail.com>
2021-10-26 16:46:47 +0800

    20211026 Added Cards and Carditem Components.

---
+++ src/components/Cards.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Carditem from "./Carditem";
+import "./Cards.css";
+
+const Cards = () => {
+  return (
+    <div className="cards">
+      <h1>Check out these EPIC Destinations!</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <Carditem
+              src="images/img-9.jpg"
+              text="Explore the hidden waterfall deep inside the Amazon Jungle"
+              label="Adventure"
+              path="/services"
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import "../../App.css"
-import Hero from "../Hero"
+import React from "react";
+import "../../App.css";
+import Hero from "../Hero";
+import Cards from "../Cards";
 
 const Home = () => {
-    return (
-        <>
-            <Hero />
-        </>
-    )
-}
+  return (
+    <>
+      <Hero />
+      <Cards />
+    </>
+  );
+};
 
-export default Home
+export default Home;
